061f3cc385448af9ceff4513d58d50bf2444a8e0
mauricio.sierrac <2376@holbertonschool.com>
2021-01-29 17:22:01 +0000

    fix betty task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,18 +13,15 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < (size - 1); j++)
+		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j +1];
+				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				print_array(array, size);	
+				print_array(array, size);
 			}
 		}
-		
 	}
-
-
 }
