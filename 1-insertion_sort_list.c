0dac52b6586f2cf3bbff8ee343b9f8e44d8da0f7
David Alzate <illker@hotmail.com>
2021-02-01 18:09:33 +0000

    :hankey:

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of ints
+ * @list: doubly linked list
+ * Return: void print the list
+ */
+
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
