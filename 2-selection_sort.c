afee2e190df355642a7a21a6dd34adca0cf0d022
mauricio.sierrac <2376@holbertonschool.com>
2021-02-01 14:51:44 +0000

    fix betty task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,25 +12,28 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, k, tmp;
 	int p_min, pos;
 
-	for (j = 0; j < size; j++)
+	if (array)
 	{
-		p_min = array[j];
-		pos = 0;
-		for (i = j; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
-			if (p_min > array[i])
+			p_min = array[j];
+			pos = 0;
+			for (i = j; i < size; i++)
 			{
-				p_min = array[i];
-				k = i;
-				pos = 1;
+				if (p_min > array[i])
+				{
+					p_min = array[i];
+					k = i;
+					pos = 1;
+				}
 			}
+			if (pos == 1)
+			{
+				tmp = array[j];
+				array[j] = array[k];
+				array[k] = tmp;
+			}
+			print_array(array, size);
 		}
-		if (pos == 1)
-		{
-			tmp = array[j];
-			array[j] = array[k];
-			array[k] = tmp;
-		}
-		print_array(array, size);
 	}
 }
