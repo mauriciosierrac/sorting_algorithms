9c83e6418f2622d0c8d23c2555650757728cc0a2
David Alzate <illker@hotmail.com>
2021-02-01 20:26:42 +0000

    insertion function :crystal_ball:

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,6 +9,26 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	if (!list)
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
 		return;
+	burger = *list;
+	while (beer)
+	{
+		beer = (*list)->next;
+
+		while (burger->prev && (burger->n < burger->prev->n))
+		{
+			burger->prev->next = burger->next;
+			if (burger->next)
+				burger->next->prev = burger->prev;
+			burger->prev = burger->next->prev;
+			burger->next = burger->prev;
+			if (burger->prev)
+				burger->prev->next = burger;
+			else
+				*list = burger;
+			burger->next->prev = burger;
+			print_list(*list);
+		}
+	}
 }
